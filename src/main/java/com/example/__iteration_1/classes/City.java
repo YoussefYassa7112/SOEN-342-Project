3485d0b04c050a8a3f007c38ae508ca693a18105
YoussefYassa7112 <149968920+YoussefYassa7112@users.noreply.github.com>
2025-11-05 01:38:35 -0500

    Added database connection

--- src/main/java/com/example/__iteration_1/classes/City.java
+++ src/main/java/com/example/__iteration_1/classes/City.java
@@ -1,11 +1,22 @@
 package com.example.__iteration_1.classes;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class City {
 
+    @Id
+    @Column(name = "city_id")
     private String name;
 
     public City(String name) {
         this.name = name;
+    }
+
+    public City() {
+
     }
 
     public String getName() {
